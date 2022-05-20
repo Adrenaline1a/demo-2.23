83f31e903e27fcdccca6d38e45a6cb2b8318bc14
Adrenaline1a <78856293+Adrenaline1a@users.noreply.github.com>
2022-05-20 09:34:32 +0300

    Отчёт

--- Задание.py
+++ Задание.py
@@ -7,8 +7,15 @@ from threading import Thread
 from time import sleep
 
 
-def func1():
-    x = 0.3
+def control_x(x):
+    return -1*x
+
+
+def control_y(x):
+    return x
+
+
+def func1(x):
     y = math.cos(x)
     EPS = 1e-07
     a = 1
@@ -19,11 +26,10 @@ def func1():
         n += 1
         print(f"Функция 1 - S={S}")
         sleep(0.4)
-    print(f"Вывод функция 1:  S={S}")
+    return S
     
 
-def func2():
-    x = 0.4
+def func2(x):
     y = math.log(x+1)
     EPS = 1e-07
     a = 1
@@ -34,11 +40,18 @@ def func2():
         n += 1
         print(f"Функция 2 - S={S}")
         sleep(0.4)
-    print(f"Вывод функция 2: S={S}")
+    return S
+
+
+def compare(first, second, x):
+    result = first(x) - second(x)
+    print(f"Результат сравнения {result}")
 
 
 if __name__ == "__main__":
-    th1 = Thread(target=func1)
-    th2 = Thread(target=func2)
+    th1 = Thread(target=compare(func1, control_x, 0.3))
+    th2 = Thread(target=compare(func2, control_y, 0.4))
     th1.start()
     th2.start()
+    th1.join()
+    th2.join()
